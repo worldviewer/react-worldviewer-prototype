776c93419ff0282ae3489067d794d810167765ed
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-27 14:03:16 -0800

    Bubble-Numbers done

--- src/Bubble.js
+++ src/Bubble.js
@@ -3,20 +3,49 @@ import React from 'react';
 let Bubble = function statelessFunctionComponentClass(props) {
 	let source = require('../graphics/' + props.source);
 
-	let style = {
-		position: 'absolute',
+	let divStyle = {
 		left: props.left,
+		position: 'absolute',
 		top: props.top,
 		width: props.width
-	}
+	};
+
+	console.log('left: ' + props.numleft);
+	console.log('top: ' + props.numtop);
+
+	let bubbleNumberStyle = {
+		backgroundColor: '#edf5f1',
+		borderRadius: '3vw',
+		height: '3vw',
+		left: props.numleft,
+		top: props.numtop,
+		position: 'absolute',
+		width: '3vw'
+	};
+
+	let imgStyle = {
+		width: '100%'
+	};
+
+	let bubbleNumber = props.num;
 
 	return (
-		<img
-			src={source}
-			style={style}
-			alt="Figure"
-			className={"Bubble " + props.source.split('.')[0]}
-		/>
+		<div style={divStyle}>
+			<img
+				alt="Figure"
+				className={"Bubble" + bubbleNumber}
+				src={source}
+				style={imgStyle}
+			/>
+
+			<div
+				alt="Slide Number"
+				className="Bubble-Number"
+				style={bubbleNumberStyle}
+			>
+				<p>{bubbleNumber + 1}</p>
+			</div>
+		</div>
 	);
 };
 
--- src/ControversyCard.js
+++ src/ControversyCard.js
@@ -7,14 +7,14 @@ import './ControversyCard.scss';
 class ControversyCard extends Component {
 	render() {
 		let bubbles = [
-			{source: 'bubble0.png', left: '7vw', top: '23vw', width: '24vw'},
-			{source: 'bubble1.png', left: '6vw', top: '55vw', width: '14vw'},
-			{source: 'bubble2.png', left: '21vw', top: '50vw', width: '10vw'},
-			{source: 'bubble3.png', left: '37vw', top: '33vw', width: '12vw'},
-			{source: 'bubble4.png', left: '52vw', top: '27vw', width: '16vw'},
-			{source: 'bubble5.png', left: '70vw', top: '36vw', width: '9vw'},
-			{source: 'bubble6.png', left: '69vw', top: '46vw', width: '9vw'},
-			{source: 'bubble7.png', left: '78vw', top: '49vw', width: '16vw'}
+			{source: 'bubble0.png', left: '7vw', top: '23vw', width: '24vw', numleft: '20vw', numtop: '4.5vw'},
+			{source: 'bubble1.png', left: '6vw', top: '55vw', width: '14vw', numleft: '5.5vw', numtop: '-1vw'},
+			{source: 'bubble2.png', left: '21vw', top: '50vw', width: '10vw', numleft: '1vw', numtop: '-0.5vw'},
+			{source: 'bubble3.png', left: '37vw', top: '33vw', width: '12vw', numleft: '-1vw', numtop: '3vw'},
+			{source: 'bubble4.png', left: '52vw', top: '27vw', width: '16vw', numleft: '14vw', numtop: '6.5vw'},
+			{source: 'bubble5.png', left: '70vw', top: '36vw', width: '9vw', numleft: '0vw', numtop: '0vw'},
+			{source: 'bubble6.png', left: '69vw', top: '46vw', width: '9vw', numleft: '0.5vw', numtop: '6.5vw'},
+			{source: 'bubble7.png', left: '78vw', top: '49vw', width: '16vw', numleft: '11vw', numtop: '0.5vw'}
 		];
 
 		return (
@@ -27,9 +27,12 @@ class ControversyCard extends Component {
 
 				{ bubbles.map( (el,i) => 
 					<Bubble
-						key={'bubble' + i}
-						source={el.source}
+						key={i}
 						left={el.left}
+						num={i}
+						numleft={el.numleft}
+						numtop={el.numtop}
+						source={el.source}
 						top={el.top}
 						width={el.width}
 					/>
