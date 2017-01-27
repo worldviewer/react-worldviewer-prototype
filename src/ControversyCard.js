d1b26e3d7032975c7e3c956db0239b1ddeef2d08
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-27 11:24:46 -0800

    Bubbles and Icon are now placed

--- src/ControversyCard.js
+++ src/ControversyCard.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import base from '../graphics/halton-arp-the-modern-galileo-bbal-card.jpg';
 import Bubble from './Bubble';
+import Icon from './Icon';
 import './ControversyCard.scss';
 
 class ControversyCard extends Component {
 	render() {
 		let bubbles = [
-			{source: 'bubble0.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble1.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble2.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble3.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble4.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble5.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble6.png', left: '0vw', top: '0vw', width: '20vw'},
-			{source: 'bubble7.png', left: '0vw', top: '0vw', width: '20vw'}
+			{source: 'bubble0.png', left: '7vw', top: '23vw', width: '24vw'},
+			{source: 'bubble1.png', left: '6vw', top: '55vw', width: '14vw'},
+			{source: 'bubble2.png', left: '21vw', top: '50vw', width: '10vw'},
+			{source: 'bubble3.png', left: '37vw', top: '33vw', width: '12vw'},
+			{source: 'bubble4.png', left: '52vw', top: '27vw', width: '16vw'},
+			{source: 'bubble5.png', left: '70vw', top: '36vw', width: '9vw'},
+			{source: 'bubble6.png', left: '69vw', top: '46vw', width: '9vw'},
+			{source: 'bubble7.png', left: '78vw', top: '49vw', width: '16vw'}
 		];
 
 		return (
@@ -33,6 +34,12 @@ class ControversyCard extends Component {
 						width={el.width}
 					/>
 				)}
+
+				<Icon
+					left='78vw'
+					top='67vw'
+					width='13vw'
+				/>
 			</div>
 		);
 	}
---
+++ src/Icon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+let Icon = function statelessFunctionComponentClass(props) {
+	let source = require('../graphics/icon.png');
+
+	let style = {
+		position: 'absolute',
+		left: props.left,
+		top: props.top,
+		width: props.width
+	}
+
+	return (
+		<img
+			src={source}
+			style={style}
+			alt="Figure"
+			className="Icon"
+		/>
+	);
+};
+
+export default Icon;
