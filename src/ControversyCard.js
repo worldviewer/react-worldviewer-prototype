1263f127381c9b16152491fd6a99671cfa0f9d6a
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-26 21:57:02 -0800

    Fixed SASS and title now done

--- src/ControversyCard.js
+++ src/ControversyCard.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import base from '../graphic/halton-arp-the-modern-galileo-bbal-card.jpg';
-import './ControversyCard.css';
+import './ControversyCard.scss';
 
 class ControversyCard extends Component {
 	render() {
 		return (
-			<div className="Controversy-Card">
+			<div className="Card">
 				<img src={base} className="Base-Layer"
 					alt="Controversy Card: Halton Arp, the Modern Galileo" />
-				<p className="Title-Left">Halton<br/>Arp</p>
-				<p className="Title-Right">The<br/>Modern<br/>Galileo</p>
+				<p className="Title Left">Halton<br/>Arp</p>
+				<p className="Title Right">The<br/>Modern<br/>Galileo</p>
 			</div>
 		);
 	}
