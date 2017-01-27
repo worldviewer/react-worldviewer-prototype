5bfdb19c90a06124d7bfd04322c381c78d4c533f
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-26 22:59:02 -0800

    Finished summary, added faded white color to text

--- src/ControversyCard.js
+++ src/ControversyCard.js
@@ -10,6 +10,7 @@ class ControversyCard extends Component {
 					alt="Controversy Card: Halton Arp, the Modern Galileo" />
 				<p className="Title Left">Halton<br/>Arp</p>
 				<p className="Title Right">The<br/>Modern<br/>Galileo</p>
+				<p className="Summary">He Was a Professional Astronomer Who<br/>Began his Career as Edwin Hubble's Assistant / While Compiling a List of Peculiar Galaxies, Arp Discovered that High-Redshift Quasars are Commonly Associated with or Even Connected by Filaments to Lower-Redshift Galaxies / Since the Big Bang Requires that Differences in Redshift Place the Objects at Different Locations, Astronomers Commonly Reject Arp's Claims /<br/>But if he is Right, then there Was No Big Bang</p>
 			</div>
 		);
 	}
