5b1c67ff59fec1dcddc59bb907314d2a0bf5ac4e
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-04-12 14:49:38 -0700

    Added more structure, share icon to FeedCard

--- src/FeedCard/FeedCardStateless.jsx
+++ src/FeedCard/FeedCardStateless.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './FeedCard.scss';
+import share from '../../public/share.svg';
 
 const FeedCardStateless = React.createClass({
 	render: function() {
 		return (
-			<div>
-				<div className="feed-image">
-
+			<div className="feedcard">
+				<div className="image">
+					<div className="share">{share}</div>
 				</div>
+				<div className="title">Were dark matter filaments "predicted"?</div>
+				<div className="breadcrumbs">The History of the Birkeland Current > Clash of Worldviews > Noteworthy Online Discussions</div>
+				<div className="ratings"></div>
+				<div className="author">
+					<div className="avatar"></div>
+					<div className="info">
+						<div className="name"></div>
+						<div className="role"></div>
+					</div>
+				</div>
+				<div className="expand-content"></div>
 			</div>
 		);
 	}
